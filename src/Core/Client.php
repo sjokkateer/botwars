6af5e67beac8438a0075b99f76d5c9c484893edd
Remy Bos <remy90@hotmail.com>
2021-06-28 17:08:04 +0200

    CLIENT: Add construction logic to client

--- src/Core/Client.php
+++ src/Core/Client.php
@@ -10,7 +10,18 @@ class Client
         $info = $gameReader->read('php://stdin');
 
         $gameData = new GameData(...explode(',', $info['game_data']));
-        $map = new Map(explode(',', $info['map_state']));
+
+        $map = new Map;
+        $mapData = explode(',', $info['map_state']);
+
+        foreach ($mapData as $data) {
+            $creationData = explode('-', $data);
+            [$type, $coordinates, $health] = [$creationData[0], explode(':', $creationData[1]), $creationData[2]];
+
+            $robot = AbstractRobot::create($type, new Coordinate(...$coordinates), $health);
+            $map->placeOnMap($robot);
+        }
+
         $userData = $info['user_data'];
 
         echo (new Game($gameData, $map, $userData))->play();
